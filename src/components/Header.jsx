66c62a98eca26460a943afd24c88c29c2fadaef0
mdave94 <molnardavid2002@gmail.com>
2022-07-05 14:55:38 +0200

    Unnecessary files are deleted. Header.jsx and App.js are updated.

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Header = ({text,bgColor,textColor}) => {
+    const headerStyles={
+        backgroundColor: bgColor,
+        color:textColor
+    }
+
+
+
+  return (
+    <header style={headerStyles} > 
+      <div className='container'>
+       
+        <p>{text}</p>
+      </div>
+    </header>
+  )
+}
+
+Header.defaultProps = {
+    text : "FeedBack APP",
+    bgColor:'rgba(0,0,0,0.4)',
+    textColor: '#ff6a95',
+
+}
+
+
+Header.propTypes ={
+    text: PropTypes.string,
+    bgColor: PropTypes.string,
+    textColor:PropTypes.string
+}
+
+export default Header
