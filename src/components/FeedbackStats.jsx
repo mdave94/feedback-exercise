dda2bbc6077458c1cc095824b1317c4c39ce1d8f
mdave94 <molnardavid2002@gmail.com>
2022-07-12 13:43:11 +0200

    FeedbackStats.jsx added

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const FeedbackStats = () => {
+const FeedbackStats = ({feedback}) => {
+    //The Reduce method is a method in the javascript array used to reduce the number of elements in an array.
+    //the second param 0 is the initial value
+    let calculatedAverage = feedback.reduce((summ, element)=>{
+        return summ + element.rating
+    },0) / feedback.length
+
+    
+    calculatedAverage = calculatedAverage.toFixed(1)
+
   return (
-    <div>
-      <h1>Stats</h1>
+    <div className='feedback-stats'>
+        <h4>{feedback.length} Feedback left</h4>
+        <h4> Average rating:
+            {isNaN(calculatedAverage) ? 0 : calculatedAverage}
+        </h4>
+        
     </div>
   )
+}
+
+FeedbackStats.propTypes = {
+    feedback: PropTypes.array.isRequired
 }
 
 export default FeedbackStats
