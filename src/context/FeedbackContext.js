2ff62ede7f19df79630c201d876697f2218b77cb
mdave94 <molnardavid2002@gmail.com>
2022-07-22 14:12:32 +0200

    update and delete fetch is done

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -33,17 +33,31 @@ export const FeedbackProvider = ({children}) =>  {
     }
 
     //update feedback Item 
-    const updateFeedback = (id, updatedItem) =>{
+    const updateFeedback = async (id, updatedItem) =>{
+      const response = await fetch(`http://localhost:5000/feedback/${id}`,
+        {
+            method:'PUT',
+            headers: {
+                'Content-Type': 'applycation/json',
+            },
+            body: JSON.stringify(updatedItem)
+        })
+        const data = await response.json()
 
-        setFeedback(feedback.map((item)=> item.id === id ? {...item,...updatedItem} : item))
+        setFeedback(feedback.map((item)=> item.id === id ? {...item,...data} : item))
 
     }
 
 
     // I can't delete directly from the data. 
     // I have to filter out the selected object and save the new list
-    const deleteFeedback = (id)=>{
+    const deleteFeedback = async (id)=>{
+
         if(window.confirm('Are You sure ? ')){
+            await fetch(`http://localhost:5000/feedback/${id}`,
+            {
+                method:'DELETE',
+            })
           setFeedback(feedback.filter((item)=> item.id !== id) )
         }
     
